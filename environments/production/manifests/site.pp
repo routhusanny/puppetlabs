cb58aa1a03dddcd513c5c9bd524d051c08f431a0
routhusanny <routhusanny@gmail.com>
2017-05-23 17:23:15 +0530

    test

--- environments/production/manifests/site.pp
+++ environments/production/manifests/site.pp
@@ -1,4 +1,29 @@
+node 'node1' {
+        file {'/root/second.txt':
+               ensure => present,
+               content => "$selinux \n $os.hardware",
+       } 
+include module1
+include module1::a::b
+include lint
+include users::user11
+include users::user21
+include group::group11
+include group::group21
+include webserver::apache
+include ntp::ntpconf
+}
+
+node default {
+}
+
 file {'/root/first.txt':
        ensure => present,
-       content => 'sample manifest'
+       content => " ${facts['os']['hardware']} ,$hostname " ,
        }
+
+node 'node2' {
+include module1::a::b
+include webserver::apache
+include ntp::ntpconf
+}
