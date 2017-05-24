0270e6bc39136eabc49a75b656d58f55bc42fe52
routhusanny <routhusanny@gmail.com>
2017-05-24 17:38:57 +0530

    test

--- environments/production/manifests/site.pp
+++ environments/production/manifests/site.pp
@@ -12,8 +12,14 @@ include group::group11
 include group::group21
 include webserver::apache
 include ntp::ntpconf
+#include variables::overwrite
+include variables::overwrite1
+include variables::inheritance1
 }
 
+class {'variables::overwrite1':
+    var3 => 'overwritten value' ,
+}
 node default {
 }
 
